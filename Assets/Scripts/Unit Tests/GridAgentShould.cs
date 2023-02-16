628b890e671212fc5dfd343fce44e55537be1d56
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-02-16 09:59:09 -0300

    ao instanciar um grid agent, sua posicao inicial esta relativa a grid

--- Assets/Scripts/Unit Tests/GridAgentShould.cs
+++ Assets/Scripts/Unit Tests/GridAgentShould.cs
@@ -19,22 +19,31 @@ namespace Unit_Tests {
             
             Assert.AreEqual(new Vector2Int(2,2), gridAgentInitialGridPos);
         }
-        
-        // [Test]
-        // public void Return_World_Pos_As
+
+        [Test]
+        public void Return_World_Pos_As_7x_7y() {
+            var agentWorldPos = _gridAgent.WorldPosition;
+            
+            Assert.AreEqual(new Vector2(7,7), agentWorldPos);
+        }
     }
 }
 
 public class GridAgent {
     private Vector2Int _currentGridPosition;
     private GridEntity _gridEntity;
+    private Vector2 _worldPosition;
 
     public GridAgent(GridEntity gridEntity, Vector2Int initialGridPosition) {
         _currentGridPosition = initialGridPosition;
         _gridEntity = gridEntity;
+
+        _worldPosition = _gridEntity.GetCellWorldPos(initialGridPosition.x, initialGridPosition.y);
     }
 
     public Vector2Int CurrentGridPosition => _currentGridPosition;
+
+    public Vector2 WorldPosition => _worldPosition;
 
     public GridEntity GridEntity => _gridEntity;
 }
