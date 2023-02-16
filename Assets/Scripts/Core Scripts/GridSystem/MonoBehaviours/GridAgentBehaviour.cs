073e186e55fae5d762f1466aaf7a17757a85956e
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-02-16 18:54:13 -0300

    agr o player se move com o teclado

--- Assets/Scripts/Core Scripts/GridSystem/MonoBehaviours/GridAgentBehaviour.cs
+++ Assets/Scripts/Core Scripts/GridSystem/MonoBehaviours/GridAgentBehaviour.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Core_Scripts.GridSystem.MonoBehaviours {
     public class GridAgentBehaviour : MonoBehaviour {
         public GridAgent GridAgent;
         public GridEntity GridEntity;
         public GridBehaviour GridBehaviour;
+        private Vector2Int _gridDirection;
 
         private void Start() {
             var thisPostion = transform.position;
@@ -15,7 +19,26 @@ namespace Core_Scripts.GridSystem.MonoBehaviours {
             GridAgent = new GridAgent(GridEntity, gridPos);
             transform.position = GridAgent.WorldPosition;
         }
+
+        private IEnumerator MoveCoroutine() {
+            while (true) {
+                MoveAgentToDirection(_gridDirection);
+                yield return new WaitForSeconds(1);
+            }
+        }
         
+        public void ReadInputAndMove(InputAction.CallbackContext ctx) {
+            if(ctx.performed) {
+                var directionRed = ctx.ReadValue<Vector2>();
+                 _gridDirection = new Vector2Int((int) -directionRed.y, (int)directionRed.x);
+                 StartCoroutine(MoveCoroutine());
+            }
+
+            if (_gridDirection == Vector2Int.zero) {
+                StopAllCoroutines();
+            }
+
+        }
         public void MoveAgentToDirection(Vector2Int direction) {
             GridAgent.MoveAgentToDirection(direction);
             transform.position = GridAgent.WorldPosition;
