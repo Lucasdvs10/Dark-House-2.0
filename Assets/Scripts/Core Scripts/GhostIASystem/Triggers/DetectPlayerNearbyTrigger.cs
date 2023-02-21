a0a20681a0abf3390ca8822c6a2a47c7f2d2062c
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-02-21 18:36:09 -0300

    criei um trigger que muda de estado quando o player estiver dentro da distancia determinada

---
+++ Assets/Scripts/Core Scripts/GhostIASystem/Triggers/DetectPlayerNearbyTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Core_Scripts.GhostIASystem {
+    public class DetectPlayerNearbyTrigger : BaseTrigger {
+        [SerializeField] private float distanceToPerceivePlayer;
+        private Transform _playerTransform;
+
+        protected void Awake() {
+            base.Awake();
+            _playerTransform = GameObject.FindWithTag("Player").transform;
+        }
+
+        private void Update() {
+            if (Vector2.Distance(transform.position, _playerTransform.position) <= distanceToPerceivePlayer) {
+                EmmitEventToChangeState();
+            }
+        }
+    }
+}
