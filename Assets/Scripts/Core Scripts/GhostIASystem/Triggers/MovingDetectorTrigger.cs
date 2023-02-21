556700e158940718072ee8eaa2b11129a9c291d1
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-02-21 18:27:51 -0300

    refatorei o moving trigger e extraí uma superclasse dele. agr todos os trigger irão herdar da classe BaseTrigger

--- Assets/Scripts/Core Scripts/GhostIASystem/Triggers/MovingDetectorTrigger.cs
+++ Assets/Scripts/Core Scripts/GhostIASystem/Triggers/MovingDetectorTrigger.cs
@@ -1,37 +1,32 @@
 ﻿using GameScripts.GameEvent;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Serialization;
 
 namespace Core_Scripts.GhostIASystem {
-    public class MovingDetectorTrigger : MonoBehaviour {
-        public UnityEvent<Transform> PlayerDetectedEventToEmmit;
-        public Transform NextState;
+    public class MovingDetectorTrigger : BaseTrigger {
         
         [SerializeField] private float perceivePlayerRadious;
         [SerializeField] private SOBaseGameEvent playerMovedEventToListen;
 
         private Transform _playersTransform;
-        private IStateManager _stateManager;
         
         private void Awake() {
+            base.Awake();
             _playersTransform = GameObject.FindWithTag("Player").transform;
-            _stateManager = GetComponentInParent<IStateManager>();
         }
 
         private void OnEnable() {
+            base.OnEnable();
             playerMovedEventToListen.Subscribe(PlayerMovedEventRaised);
-            PlayerDetectedEventToEmmit.AddListener(_stateManager.ChangeState);
         }
 
         private void OnDisable() {
+            base.OnDisable();
             playerMovedEventToListen.Unsubscribe(PlayerMovedEventRaised);
-            PlayerDetectedEventToEmmit.RemoveListener(_stateManager.ChangeState);
         }
 
         private void PlayerMovedEventRaised() {
             if (Vector2.Distance(_playersTransform.position, transform.position) <= perceivePlayerRadious) {
-                PlayerDetectedEventToEmmit.Invoke(NextState);
+                EmmitEventToChangeState();
             }
         }
 
