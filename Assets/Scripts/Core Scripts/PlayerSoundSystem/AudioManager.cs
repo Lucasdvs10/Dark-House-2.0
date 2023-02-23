2eea5499699af8ab72f97899e3299a8ff8db2b33
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-02-23 11:41:51 -0300

    versao rudimentar do sistema de som do player feita

--- Assets/Scripts/Core Scripts/PlayerSoundSystem/AudioManager.cs
+++ Assets/Scripts/Core Scripts/PlayerSoundSystem/AudioManager.cs
@@ -8,16 +8,29 @@ namespace Core_Scripts.PlayerSoundSystem {
         [SerializeField] private SOBaseGameEvent _whenToEmitSoundEvent;
         [SerializeField] private SOVec3Singleton _playerDesiredPos;
         [SerializeField] private AudioSource _audioEmmiter;
-        private Dictionary<Vector2Int, string> _audioMap;
+
+        [SerializeField] private AudioClip[] _audiosArray;
+        [SerializeField] private string[] _tileNamesArray;
+        private Dictionary<string, AudioClip> _audioMap = new Dictionary<string, AudioClip>();
 
         private void Awake() {
-            _whenToEmitSoundEvent.Subscribe(TeleportSoudEmmiterToDesiredPos);
+            _whenToEmitSoundEvent.Subscribe(TeleportSoundEmmiterToDesiredPos);
+            for (int i = 0; i < _audiosArray.Length; i++) {
+                _audioMap.Add(_tileNamesArray[i], _audiosArray[i]);
+            }
         }
-
-        public void TeleportSoudEmmiterToDesiredPos() {
+        
+        //todo: Ao invés de só injetar o áudio, criar uma fila e adicioná-lo na fila (dessa maneira, não precisaremos cortar nenhum áudio no meio)
+        //todo: Criar um som padrão para tiles ainda não cadastrados
+        public void PlaySoundEffect(string tileName) {
+            _audioEmmiter.clip = _audioMap[tileName];
+            _audioEmmiter.Play();
+        }
+        
+        public void TeleportSoundEmmiterToDesiredPos() {
             _audioEmmiter.transform.position = _playerDesiredPos.Value;
         }
 
-        public Dictionary<Vector2Int, string> AudioMap => _audioMap;
+        public Dictionary<string, AudioClip> AudioMap => _audioMap;
     }
 }
