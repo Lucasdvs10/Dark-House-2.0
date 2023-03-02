91b02d3213ee9f2b0d5f8fee88376946e5616b47
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-03-02 08:52:14 -0300

    Csharp puro do Sound queue generator feito

---
+++ Assets/Scripts/Core Scripts/BattleSystem/PlayerValidatorCommand/SoundQueueGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unit_Tests {
+    public class SoundQueueGenerator {
+        private List<string> _allSoundsList;
+
+        public Queue<string> GenerateSoundQueueToDefeat(int queueLength) {
+            var queueToReturn = new Queue<string>();
+
+            for (int i = 0; i < queueLength; i++) {
+                var randomSound = GetRandomItemFromList<string>(_allSoundsList);
+                queueToReturn.Enqueue(randomSound);
+            }
+            return queueToReturn;
+        }
+
+        private T GetRandomItemFromList<T>(List<T> listToPick) {
+            var random = new Random();
+            var randomIndex = random.Next(listToPick.Count);
+
+            return listToPick[randomIndex];
+        }
+        public SoundQueueGenerator(List<string> allSoundsList) {
+            _allSoundsList = allSoundsList;
+        }
+    }
+}
