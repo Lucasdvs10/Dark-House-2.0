e36adb639195e793c292dcbdfd9c5693a64b6c25
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-03-16 09:24:23 -0300

    sistema de chaves implementado

---
+++ Assets/Scripts/Core Scripts/GridSystem/MonoBehaviours/CenterObjectOnStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Core_Scripts.GridSystem.MonoBehaviours {
+    public class CenterObjectOnStart : MonoBehaviour {
+        private GridAgent _gridAgent;
+        private GridEntity _gridEntity;
+        private GridBehaviour _gridBehaviour;
+
+        private void Start() {
+            _gridBehaviour = FindObjectOfType<GridBehaviour>();
+            
+            var thisPostion = transform.position;
+            _gridEntity = _gridBehaviour.GridEntity;
+
+            var gridPos = new Vector2Int(_gridEntity.GetCellFromWorldPos(thisPostion).row,
+                _gridEntity.GetCellFromWorldPos(thisPostion).col);
+            _gridAgent = new GridAgent(_gridEntity, gridPos);
+            transform.position = _gridAgent.WorldPosition;
+        }
+    }
+}
