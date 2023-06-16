71292d7c3d087035ad822ac693c1da30cebaf570
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-06-15 21:46:36 -0300

    #Correções de bug prototipo 2 - 1
    - Corrigi um bug em que ao apertar o contador várias vezes, ele atualizava o som. Ou seja, ele mostrava sons diferentes para a mesma quantidade de teclas
    
    - Velocidade de movimento do fantasma reduzida de 2.5 para 1.75

--- Assets/Scripts/Core Scripts/KeySystem/KeySoundCounter.cs
+++ Assets/Scripts/Core Scripts/KeySystem/KeySoundCounter.cs
@@ -9,7 +9,6 @@ namespace Core_Scripts.KeySystem {
         [SerializeField] private AudioClip[] _audioClipsArray;
         private KeyManager _keyManager;
         private AudioSource _audioSource;
-        private int _currentIndex;
 
         private void Awake() {
             _keyManager = GetComponent<KeyManager>();
@@ -24,32 +23,20 @@ namespace Core_Scripts.KeySystem {
             _keyCounterPressedEvent.Unsubscribe(ShowKeysColectedAmount);
         }
         
-        //Todo: Tem um bugzinho em que se eu apertar pra contar duas vezes, na segunda vez ele toca o som de onde ele parou
-        //Sinceramente eu acho que gostei disso
         public void ShowKeysColectedAmount() {
             print($"Eu já coletei {_keyManager.KeysColectedAmount} chaves!");
             StartCoroutine(PlayAudiosCoroutine(_keyManager.KeysColectedAmount));
         }
 
         private IEnumerator PlayAudiosCoroutine(int audiosToPlay) {
-            int audiosPlayed = 0;
-            while (audiosPlayed < audiosToPlay) {
-                _audioSource.clip = _audioClipsArray[_currentIndex];
+            int currentIndexToPlay = 0;
+            while (currentIndexToPlay < audiosToPlay) {
+                _audioSource.clip = _audioClipsArray[currentIndexToPlay];
                 _audioSource.Play();
                 
                 yield return new WaitUntil(() => !_audioSource.isPlaying);
 
-                audiosPlayed++;
-                AddOneToIndex();
-            }
-        }
-        
-        
-        private void AddOneToIndex() {
-            _currentIndex++;
-
-            if (_currentIndex >= _audioClipsArray.Length) {
-                _currentIndex = 0;
+                currentIndexToPlay++;
             }
         }
     }
