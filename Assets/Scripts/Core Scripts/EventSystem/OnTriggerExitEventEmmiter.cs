bf3fb30cc6b1df5bfb86c7b73d325a81e0b723d6
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2023-07-12 14:42:43 -0300

    Montei a fase de tutorial
    A fase de tutorial está montada, porém está com todos os áudios mockados

---
+++ Assets/Scripts/Core Scripts/EventSystem/OnTriggerExitEventEmmiter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GameScripts.GameEvent {
+    public class OnTriggerExitEventEmmiter : MonoBehaviour {
+        [SerializeField] private SOBaseGameEvent _eventToEmmit;
+
+        private void OnTriggerExit2D(Collider2D col) {
+            _eventToEmmit.InvokeEvent();
+        }    }
+}
