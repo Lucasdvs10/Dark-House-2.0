d1379f469ce7a7e1fca1b7405b8363ba9afb87ae
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2025-03-12 10:05:27 -0300

    Timer de 10 mins implementado

---
+++ Assets/Scripts/Core Scripts/GameManager/LevelTimer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using GameScripts.GameEvent;
+using UnityEngine;
+
+namespace Core_Scripts {
+    public class LevelTimer : MonoBehaviour {
+        [SerializeField] private int _timeLimitInSeconds = 600;
+        [SerializeField] private SOBaseGameEvent _gameOverEvent;
+        private int _elapsedTimeInSeconds;
+        private float _startTime;
+
+        [ContextMenu("Start Timer")]
+        public void StartTimer() {
+            _startTime = Time.time;
+            StartCoroutine(StartTimerCO());
+        }
+        
+        [ContextMenu("Resume Timer")]
+        public void ResumeTimer() {
+            StartCoroutine(StartTimerCO());
+        }
+        
+        [ContextMenu("Pause Timer")]
+        public void PauseTimer() {
+            _elapsedTimeInSeconds = Mathf.RoundToInt(Time.time - _startTime);
+            StopAllCoroutines();
+            
+            Debug.Log($"Tempo passado: {_elapsedTimeInSeconds}");
+        }
+
+        IEnumerator StartTimerCO() {
+            yield return new WaitForSeconds(_timeLimitInSeconds - _elapsedTimeInSeconds);
+            _gameOverEvent.InvokeEvent();
+        }
+    }
+}
