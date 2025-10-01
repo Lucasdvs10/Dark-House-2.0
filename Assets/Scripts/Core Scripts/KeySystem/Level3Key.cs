4085fd99154ef06da7b673e260e0557809b76975
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2025-09-30 22:51:21 -0300

    Caso pegue a chave errada, é possivel se afastar

--- Assets/Scripts/Core Scripts/KeySystem/Level3Key.cs
+++ Assets/Scripts/Core Scripts/KeySystem/Level3Key.cs
@@ -9,16 +9,21 @@ namespace Core_Scripts.KeySystem {
         [SerializeField] private SOVec3Singleton _playerPositionSingleton;
         [SerializeField] private SOIntGameEvent _keyHasBeenColectedEvent;
         [SerializeField] private int _collectDistanceInCells = 1;
+        [SerializeField] private int _cellDistanceThreashold = 1;
+        [SerializeField] private SOBaseGameEvent _wrongKeyCollectedEvent;
         private int _id;
         public UnityEvent _keyColectedLocalEvent;
+        private bool _isActive = true;
 
         private const float _cellSize = 2.25f;
         private void OnEnable() {
             _playerMoveEvent.Subscribe(CheckIfHasBeenColected);
+            _wrongKeyCollectedEvent.Subscribe(DeactivateKey);
         }
 
         private void OnDisable() {
             _playerMoveEvent.Unsubscribe(CheckIfHasBeenColected);
+            _wrongKeyCollectedEvent.Unsubscribe(DeactivateKey);
         }
 
         public void SetId(int id) {
@@ -29,9 +34,16 @@ namespace Core_Scripts.KeySystem {
             var playerPosition = _playerPositionSingleton.Value;
 
             if ((playerPosition - transform.position).sqrMagnitude <= _cellSize * _collectDistanceInCells) { //Fiz desse jeito para que a chave tenha uma hitbox maior
-                _keyHasBeenColectedEvent.InvokeEvent(_id);
-                _keyColectedLocalEvent.Invoke();
+                if (_isActive) {
+                    _keyHasBeenColectedEvent.InvokeEvent(_id);
+                    _keyColectedLocalEvent.Invoke();
+                }
+            }
+            else if((playerPosition - transform.position).sqrMagnitude > _cellSize * _collectDistanceInCells + _cellDistanceThreashold && !_isActive) {
+                _isActive = true;
             }
         }
+        
+        public void DeactivateKey() => _isActive = false;
     }
 }
